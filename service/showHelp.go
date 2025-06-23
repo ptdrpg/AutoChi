dbfd2ba0755949f81d96b149246ba7cab52444e6
ptdrpg <pagielamizael@gmail.com>
2025-06-23 14:37:55 +0300

    fix(showHelp): update help

--- service/showHelp.go
+++ service/showHelp.go
@@ -7,10 +7,15 @@ import "fmt"
 var ParamsArgs = []string{"-c", "create", "-h", "--help"}
 
 func ShowHelp() {
-	fmt.Println("Utilisation : ./test [paramètre] [optionnel]")
+	fmt.Println("Utilisation : chiS [paramètre] [optionnel]")
 	fmt.Println("Paramètres disponibles :")
 	for _, flag := range ParamsArgs {
-		fmt.Println("  ", flag)
+		if flag == "-c" || flag == "create" {
+			fmt.Println(flag,": " ,"create new project")
+		}
+		if flag == "-h" || flag == "--help" {
+			fmt.Println(flag, ": ","show all command")
+		}
 	}
 }
 
